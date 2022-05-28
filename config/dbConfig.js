b252adc1b6d180258ad0540a54be287253c3cb74
Sylvie Canongia <sylvie.canongia.dev@gmail.com>
2022-05-28 13:35:42 +0200

    added dbConfig.js for connection via mongoose

---
+++ config/dbConfig.js
@@ -0,0 +1,24 @@
+import { envConfig } from "./envConfig.js";
+import mongoose from "mongoose";
+
+const { db_url, } = envConfig.db;
+
+const mongoDbString = db_url;
+
+const mongooseConnect = mongoose.connect(mongoDbString);
+
+const database = mongoose.connection;
+
+// It will connect to the database,
+// and throws any error if the connection fails
+database.on('error', (error) => {
+  console.log(`Connection error : ${error}`);
+});
+
+// it will run only one time. If it is successful,
+// it will show a message that says Database Connected.
+database.once('connected', () => {
+  console.log('Database connected');
+})
+
+export default { mongooseConnect };
